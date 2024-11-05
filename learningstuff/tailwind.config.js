abd61b98ce72e8a318193bcb399e537872c1c6ea
Ozwald-eth <obouzad@gmail.com>
2024-11-05 06:34:48 +0000

    Changes to be committed:

--- learningstuff/tailwind.config.js
+++ learningstuff/tailwind.config.js
@@ -1,17 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
