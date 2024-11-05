3c895962c2234e7688f81d83e09fc306b40578ad
Ozwald-eth <obouzad@gmail.com>
2024-11-05 06:13:45 +0000

    Changes to be committed:

--- learningstuff/src/app/layout.js
+++ learningstuff/src/app/layout.js
@@ -1,9 +1,15 @@
-import "./globals.css";
+// app/layout.js
+import "./globals.css"; // Adjust this path if needed
+
+export const metadata = {
+  title: "SHHHHHHHHHHHH",
+  description: "SHHHHHHHHHHHHHHH sush hush sush SHHHHHHHHH",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 text-gray-900">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
