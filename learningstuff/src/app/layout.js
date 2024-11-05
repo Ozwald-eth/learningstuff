acc760a173184abc96d88eaa7ed81e19438463f2
Ozwald-eth <obouzad@gmail.com>
2024-11-05 06:35:34 +0000

    Changes to be committed:

--- learningstuff/src/app/layout.js
+++ learningstuff/src/app/layout.js
@@ -1,15 +1,17 @@
 // app/layout.js
-import "../../styles/globals.css";
+import "./globals.css";
 
 export const metadata = {
-  title: "SHHHHHHHHHHHH",
-  description: "SHHHHHHHHHHHHHHH sush hush sush SHHHHHHHHH",
+  title: "Learning Scrapbook",
+  description: "A place to manage resources",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ backgroundColor: "#f9f9f9", padding: "1rem" }}>
+        {children}
+      </body>
     </html>
   );
 }
